3cb296bc3d492c2d79d3f685a74d2856893f2b25
RybickiMichal <rybickimichal2@gmail.com>
2021-01-19 18:29:53 +0100

    code cleaning

--- src/main/java/com/mprybicki/sensorservice/camerasensor/service/CameraSenderService.java
+++ src/main/java/com/mprybicki/sensorservice/camerasensor/service/CameraSenderService.java
@@ -2,7 +2,6 @@ package com.mprybicki.sensorservice.camerasensor.service;
 
 
 import com.mprybicki.sensorservice.common.model.CameraDTO;
-import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
